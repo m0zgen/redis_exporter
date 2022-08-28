06bfc456066d4273d7c554add3cc216d9e486451
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-08-28 10:58:32 +0200

    - checking service exists - set -e

--- pkg/after.sh
+++ pkg/after.sh
@@ -2,6 +2,8 @@
 # Go binary builder
 # Created by Y.G., https://sys-adm.in
 
+set -e 
+
 # Envs
 # ---------------------------------------------------\
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
@@ -10,6 +12,16 @@ SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 _USER=redis-exporter
 BINARY_DEST=/usr/local/bin/redis_exporter
 
+# Checking exists
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Checking Redis user exists
 if ! id -u "$_USER" >/dev/null 2>&1; then
     useradd -s /sbin/nologin -c "Redis Exporter User" -d /var/lib/redis -r -M ${_USER}
@@ -17,4 +29,8 @@ fi
 
 # Final steps
 chown -R -L ${_USER}:${_USER} ${BINARY_DEST}
-systemctl --system daemon-reload; systemctl enable --now redis_exporter
+systemctl --system daemon-reload
+
+if service_exists "$_USER"; then
+    systemctl enable --now redis_exporter
+fi
